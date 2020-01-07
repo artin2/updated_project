595d9ec6e97713af97ae5226dae0a2c84f28717c
artin2 <artinkasumyan@gmail.com>
2020-01-07 17:43:18 +0400

    Add basic leaflet maps

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,14 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   validates :tweet_content, :presence => true
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
+  def address
+    [street, city, zip].compact.join(", ")
+  end
+
+  def address_changed?
+    street_changed? || city_changed? || zip_changed?
+  end
 end
